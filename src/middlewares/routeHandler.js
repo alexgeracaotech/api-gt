54d6ac69602504e3d4ebbee412aa295c9462d08e
Alex Bessa <alexbessa.dev@gmail.com>
2025-06-05 16:06:46 -0300

    desestruturando parâmetro

--- src/middlewares/routeHandler.js
+++ src/middlewares/routeHandler.js
@@ -2,12 +2,19 @@
 import { routes } from "../routes.js";
 
 export const routeHandler = (request, response) => {
-  
+
   const route = routes.find(route => {
-    return route.method === request.method && route.path === request.url;
+    return route.method === request.method && route.path.test(request.url);
   });
 
-  if(route){
+  if (route) {
+
+    const routeParams = request.url.match(route.path);
+
+    const { ...params } = routeParams.groups;
+
+    console.log(params);
+
     return route.controller(request, response);
   }
 
--- src/utils/parseRoute.js
+++ src/utils/parseRoute.js
@@ -1,4 +1,18 @@
 
 export const parseRoute = (path) => {
-  console.log(path);
+  
+  const routeRegex = /:([a-zA-Z]+)/g;
+  // as barras indicam as extremidades do que será identificado
+  // os dois pontos indicam onde a detecção iniciará
+  // os parênteses indicam um grupo de caracteres
+  // os colchetes indicam a especificidade dos caracteres
+  // o sinal de adição indica para considerar tudo que vier após
+  // a letra "g" indica que será analisado todas as ocorrências identificadas
+
+  const params = path.replaceAll(routeRegex, '(?<id>[a-z0-9-_]+)');
+
+  const pathRegex = new RegExp(params);
+  
+  return pathRegex;
+
 }
